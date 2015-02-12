96fc9ec1cfdc01ff04477cbbefc4cc4fec20b0f2
pingmalu <malu@malu.me>
2015-02-12 13:28:28 +0800

    add css

--- public/links/index.php
+++ public/links/index.php
@@ -1,19 +1,461 @@
 <!DOCTYPE html>
 <html lang="zh-CN">
- <head>
-  <meta charset="utf-8" />
-  <meta content="width=device-width, initial-scale=1, minimum-scale=1" name="viewport" />
-  <meta name="description" content="Overview of Google products" />
-  <title>
-   Google 大全 - MaLu.me
-  </title>
-  <link rel="stylesheet" href="//www.google.com/about/products/default.css" type="text/css" />
- </head>
- <body>
-<?php
-
-echo 'is links';
-
-?>
- </body>
- </html>
+<head>
+ <meta charset="utf-8" />
+ <meta content="width=device-width, initial-scale=1, minimum-scale=1" name="viewport" />
+ <meta name="description" content="Overview of Google products" />
+ <title>
+  在线工具 - MaLu.me
+ </title>
+ <link rel="stylesheet" href="default.css" type="text/css" />
+</head>
+<body>
+<div class="maia-aux">
+<div class="products-section">
+    <h2>
+     网络
+    </h2>
+    <ul>
+     <li class="break">
+      <a href="//www.google.com/intl/zh-CN/insidesearch/" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/search-32.png" alt="">
+      </a>
+      <p>
+       <a href="//www.google.com/intl/zh-CN/insidesearch/" data-g-event="Product click" data-g-action="link">
+        网页搜索
+       </a>
+      </p>
+      <p>
+       在数十亿网页中搜索所需内容
+      </p>
+     </li>
+     <li>
+      <a href="/chrome?hl=zh-CN&amp;brand=CHMI" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/chrome-32.png" alt="">
+      </a>
+      <p>
+       <a href="/chrome?hl=zh-CN&amp;brand=CHMI" data-g-event="Product click" data-g-action="link">
+        Google Chrome
+       </a>
+      </p>
+      <p>
+       高速、简约、安全的浏览器
+      </p>
+     </li>
+     <li class="break">
+      <a href="/bookmarks/" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/bookmarks-32.png" alt="">
+      </a>
+      <p>
+       <a href="/bookmarks/" data-g-event="Product click" data-g-action="link">
+        书签
+       </a>
+      </p>
+      <p>
+       访问书签和加星标的项
+      </p>
+     </li>
+    </ul>
+    <h2>
+     移动
+    </h2>
+    <ul>
+     <li class="break">
+      <a href="/mobile/" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/mobile-32.png" alt="">
+      </a>
+      <p>
+       <a href="/mobile/" data-g-event="Product click" data-g-action="link">
+        移动
+       </a>
+      </p>
+      <p>
+       在手机上使用Google产品
+      </p>
+     </li>
+     <li>
+      <a href="/mobile/maps/" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/maps_for_mobile-32.png" alt="">
+      </a>
+      <p>
+       <a href="/mobile/maps/" data-g-event="Product click" data-g-action="link">
+        Google地图（移动版）
+       </a>
+      </p>
+      <p>
+       可在手机上查看地图、您的位置，并获取路线指引
+      </p>
+     </li>
+     <li class="break">
+      <a href="/mobile/search/" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/search_for_mobile-32.png" alt="">
+      </a>
+      <p>
+       <a href="/mobile/search/" data-g-event="Product click" data-g-action="link">
+        移动版搜索
+       </a>
+      </p>
+      <p>
+       随时随地在Google上搜索
+      </p>
+     </li>
+    </ul>
+    <h2>
+     媒体
+    </h2>
+    <ul>
+     <li class="break">
+      <a href="//books.google.com/books?hl=zh-CN" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/books-32.png" alt="">
+      </a>
+      <p>
+       <a href="//books.google.com/books?hl=zh-CN" data-g-event="Product click" data-g-action="link">
+        Google图书
+       </a>
+      </p>
+      <p>
+       搜索图书全文
+      </p>
+     </li>
+     <li>
+      <a href="//images.google.com/imghp?hl=zh-CN" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/images-32.gif" alt="">
+      </a>
+      <p>
+       <a href="//images.google.com/imghp?hl=zh-CN" data-g-event="Product click" data-g-action="link">
+        图片搜索
+       </a>
+      </p>
+      <p>
+       在网络上搜索图片
+      </p>
+     </li>
+     <li class="break">
+      <a href="//news.google.com/nwshp?hl=zh-CN" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/news-32.png" alt="">
+      </a>
+      <p>
+       <a href="//news.google.com/nwshp?hl=zh-CN" data-g-event="Product click" data-g-action="link">
+        新闻
+       </a>
+      </p>
+      <p>
+       搜索数以千计的新闻报道
+      </p>
+     </li>
+     <li>
+      <a href="//video.google.com/?hl=zh-CN" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/videos-32.png" alt="">
+      </a>
+      <p>
+       <a href="//video.google.com/?hl=zh-CN" data-g-event="Product click" data-g-action="link">
+        视频搜索
+       </a>
+      </p>
+      <p>
+       在网络上搜索视频
+      </p>
+     </li>
+    </ul>
+    <h2>
+     地理
+    </h2>
+    <ul>
+     <li class="break">
+      <a href="//maps.google.com/?hl=zh-CN" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/maps-32.png" alt="">
+      </a>
+      <p>
+       <a href="//maps.google.com/?hl=zh-CN" data-g-event="Product click" data-g-action="link">
+        Google地图
+       </a>
+      </p>
+      <p>
+       查看地图和路线
+      </p>
+     </li>
+     <li>
+      <a href="//www.google.com/intl/zh-CN/earth/" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/earth-32.png" alt="">
+      </a>
+      <p>
+       <a href="//www.google.com/intl/zh-CN/earth/" data-g-event="Product click" data-g-action="link">
+        地球
+       </a>
+      </p>
+      <p>
+       通过小小的计算机探索整个世界
+      </p>
+     </li>
+    </ul>
+    <h2>
+     专业搜索
+    </h2>
+    <ul>
+     <li class="break">
+      <a href="/blogsearch?hl=zh-CN" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/blogs-32.png" alt="">
+      </a>
+      <p>
+       <a href="/blogsearch?hl=zh-CN" data-g-event="Product click" data-g-action="link">
+        博客搜索
+       </a>
+      </p>
+      <p>
+       查找有关您喜爱的主题的博客
+      </p>
+     </li>
+     <li>
+      <a href="/coop/cse/?hlzh-CN" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/customsearch-32.png" alt="">
+      </a>
+      <p>
+       <a href="/coop/cse/?hlzh-CN" data-g-event="Product click" data-g-action="link">
+        自定义搜索
+       </a>
+      </p>
+      <p>
+       打造您社区自己的搜索体验
+      </p>
+     </li>
+     <li class="break">
+      <a href="//scholar.google.com/?hl=zh-CN" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/scholar-32.png" alt="">
+      </a>
+      <p>
+       <a href="//scholar.google.com/?hl=zh-CN" data-g-event="Product click" data-g-action="link">
+        学术搜索
+       </a>
+      </p>
+      <p>
+       搜索学术论文
+      </p>
+     </li>
+     <li>
+      <a href="/trends/?hl=zh-CN" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/feature/line_graph-b32.png" alt="">
+      </a>
+      <p>
+       <a href="/trends/?hl=zh-CN" data-g-event="Product click" data-g-action="link">
+        Google趋势
+       </a>
+      </p>
+      <p>
+       探索过去和现在的搜索趋势
+      </p>
+     </li>
+    </ul>
+    <h2>
+     家用与办公
+    </h2>
+    <ul>
+     <li class="break">
+      <a href="//mail.google.com/mail?hl=zh-CN#utm_source=zh-CN-et-more&amp;utm_medium=et&amp;utm_campaign=zh-CN" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/googlemail-32.png" alt="">
+      </a>
+      <p>
+       <a href="//mail.google.com/mail?hl=zh-CN#utm_source=zh-CN-et-more&amp;utm_medium=et&amp;utm_campaign=zh-CN" data-g-event="Product click" data-g-action="link">
+        Gmail
+       </a>
+      </p>
+      <p>
+       电子邮件可快速访问并能搜索，垃圾电子邮件防护机制非常高效
+      </p>
+     </li>
+     <li>
+      <a href="/drive/?utm_medium=et&amp;utm_source=about&amp;utm_campaign=et-about" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/drive-32.png" alt="">
+      </a>
+      <p>
+       <a href="/drive/?utm_medium=et&amp;utm_source=about&amp;utm_campaign=et-about" data-g-event="Product click" data-g-action="link">
+        云端硬盘
+       </a>
+      </p>
+      <p>
+       在同一位置创建、共享和保存您的所有文件和资料
+      </p>
+     </li>
+     <li class="break">
+      <a href="//docs.google.com/document?usp=about_products" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/docs-32.png" alt="">
+      </a>
+      <p>
+       <a href="//docs.google.com/document?usp=about_products" data-g-event="Product click" data-g-action="link">
+        文档
+       </a>
+      </p>
+      <p>
+       打开、修改和创建文档
+      </p>
+     </li>
+     <li>
+      <a href="//docs.google.com/spreadsheets?usp=about_products" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/spreadsheets-32.png" alt="">
+      </a>
+      <p>
+       <a href="//docs.google.com/spreadsheets?usp=about_products" data-g-event="Product click" data-g-action="link">
+        表格
+       </a>
+      </p>
+      <p>
+       打开、修改和创建电子表格
+      </p>
+     </li>
+     <li class="break">
+      <a href="//docs.google.com/presentation?usp=about_products" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/presentations-32.png" alt="">
+      </a>
+      <p>
+       <a href="//docs.google.com/presentation?usp=about_products" data-g-event="Product click" data-g-action="link">
+        幻灯片
+       </a>
+      </p>
+      <p>
+       打开、修改和创建幻灯片
+      </p>
+     </li>
+     <li>
+      <a href="//docs.google.com/forms/create?usp=about_productspage&amp;authuser=0" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/forms-32.png" alt="">
+      </a>
+      <p>
+       <a href="//docs.google.com/forms/create?usp=about_productspage&amp;authuser=0" data-g-event="Product click" data-g-action="link">
+        表单
+       </a>
+      </p>
+      <p>
+       免费制作调查问卷
+      </p>
+     </li>
+     <li class="break">
+      <a href="//docs.google.com/drawings/create?usp=about_products&amp;authuser=0" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/drawings-32.png" alt="">
+      </a>
+      <p>
+       <a href="//docs.google.com/drawings/create?usp=about_products&amp;authuser=0" data-g-event="Product click" data-g-action="link">
+        Google绘图
+       </a>
+      </p>
+      <p>
+       创建各种图表和流程图
+      </p>
+     </li>
+     <li>
+      <a href="//sites.google.com" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/sites-32.png" alt="">
+      </a>
+      <p>
+       <a href="//sites.google.com" data-g-event="Product click" data-g-action="link">
+        协作平台
+       </a>
+      </p>
+      <p>
+       创建网站和安全的群组Wiki
+      </p>
+     </li>
+     <li class="break">
+      <a href="//www.google.com/calendar/render?hl=zh-CN" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/calendar-32.png" alt="">
+      </a>
+      <p>
+       <a href="//www.google.com/calendar/render?hl=zh-CN" data-g-event="Product click" data-g-action="link">
+        Google日历
+       </a>
+      </p>
+      <p>
+       合理安排日程活动并与朋友分享
+      </p>
+     </li>
+     <li>
+      <a href="//translate.google.com/?hl=zh-CN" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/translate-32.png" alt="">
+      </a>
+      <p>
+       <a href="//translate.google.com/?hl=zh-CN" data-g-event="Product click" data-g-action="link">
+        Google翻译
+       </a>
+      </p>
+      <p>
+       即时翻译50多种语言的文字、网页和文件
+      </p>
+     </li>
+    </ul>
+    <h2>
+     社交
+    </h2>
+    <ul>
+     <li class="break">
+      <a href="https://plus.google.com/?utm_source=embedded&amp;utm_medium=googleabout&amp;utm_campaign=link" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/gplus-32.png" alt="">
+      </a>
+      <p>
+       <a href="https://plus.google.com/?utm_source=embedded&amp;utm_medium=googleabout&amp;utm_campaign=link" data-g-event="Product click" data-g-action="link">
+        Google+
+       </a>
+      </p>
+      <p>
+       如现实生活中一样分享，颠覆您以往的网络观念
+      </p>
+     </li>
+     <li>
+      <a href="//www.blogger.com/start?hl=zh-CN" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/blogger-32.png" alt="">
+      </a>
+      <p>
+       <a href="//www.blogger.com/start?hl=zh-CN" data-g-event="Product click" data-g-action="link">
+        Blogger
+       </a>
+      </p>
+      <p>
+       通过博客在线分享生活点滴 - 快捷、简单且免费
+      </p>
+     </li>
+     <li class="break">
+      <a href="//groups.google.com/grphp?hl=zh-CN" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/groups-32.png" alt="">
+      </a>
+      <p>
+       <a href="//groups.google.com/grphp?hl=zh-CN" data-g-event="Product click" data-g-action="link">
+        网上论坛
+       </a>
+      </p>
+      <p>
+       创建邮寄列表和讨论群组
+      </p>
+     </li>
+     <li>
+      <a href="/hangouts/" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/hangouts-32.png" alt="">
+      </a>
+      <p>
+       <a href="/hangouts/" data-g-event="Product click" data-g-action="link">
+        环聊
+       </a>
+      </p>
+      <p>
+       象现实生活中那样随时随地面对面聊天，而且免费
+      </p>
+     </li>
+    </ul>
+    <h2>
+     创新
+    </h2>
+    <ul>
+     <li class="break">
+      <a href="//code.google.com/intl/zh-CN/" data-g-event="Product click" data-g-action="icon">
+       <img src="//www.google.com/images/icons/product/code-32.png" alt="">
+      </a>
+      <p>
+       <a href="//code.google.com/intl/zh-CN/" data-g-event="Product click" data-g-action="link">
+        Google代码
+       </a>
+      </p>
+      <p>
+       开发者工具、API和资源
+      </p>
+     </li>
+    </ul>
+   </div>
+</div>
+</body>
+</html>
